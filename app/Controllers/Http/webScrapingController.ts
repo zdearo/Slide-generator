e74a215def68d6a734599ebf1b6bd9639c20d953
Adryel Dearo <adryelzd77@outlook.com>
2023-07-04 23:34:47 -0300

    Tratando erros possíveis das funções

--- app/Controllers/Http/webScrapingController.ts
+++ app/Controllers/Http/webScrapingController.ts
@@ -4,7 +4,8 @@ import axios from "axios";
 export default class webScrapingController {
     //Função para buscar o HTML da página e retornar o documento
     public async getDoc(url: string): Promise<string | Error> {
-        return axios.get(url)
+        return axios
+            .get(url)
             .then((response) => {
                 //Verifica a formatação da URL
                 if (!url.startsWith("https://www.letras.mus.br/")) {
@@ -15,7 +16,6 @@ export default class webScrapingController {
                 return doc; // Retorna o documento
             })
             .catch((err: Error) => {
-                console.log("Erro ao definir a URL: " + err); // Exibe mensagem de erro caso não consiga definir a URL
                 return err; // Retorna o erro
             });
     }
@@ -23,34 +23,40 @@ export default class webScrapingController {
     //Função para buscar a letra da música e retornar o título e a letra separada em um array
     public async getLetra({ request: req, response: res }): Promise<string> {
         const url: string = await req.all().url; //Recebe a URL da requisição
-        const doc = await this.getDoc(url); //Busca o documento da página utilizando a função getDoc e passando a URL como parâmetro
+        return this.getDoc(url)
+            .then((doc) => {
+                // Verifica se doc teve retorno de um erro
+                if (doc instanceof Error) {
+                    throw new Error(doc.message); // Lança um erro caso doc seja um erro
+                }
 
-        //Verifica se retornou um erro
-        if (doc instanceof Error) {
-            return "Erro ao definir a URL: " + doc.message;
-        }
+                console.log("URL definida com sucesso!");
+                const pag = cheerio.load(doc); // Carrega a página HTML
+                const letraContainer = pag("div#js-lyric-cnt div.cnt-letra p"); // Busca o container da letra da música
 
-        console.log("URL definida com sucesso!");
-        const pag = cheerio.load(doc); // Carrega a página HTML
+                //Verifica se a letra da música foi encontrada
+                if (letraContainer.length == 0) {
+                    throw new Error("Letra não encontrada!"); // Lança um erro caso a letra não seja encontrada
+                }
 
-        if (pag("div#js-lyric-cnt div.cnt-letra p").length == 0) {
-            return "Erro ao buscar a letra da música!";
-        }
+                const titulo = pag("article div.cnt-head_title h1").text(); //Busca o título da música
+                const letra: Array<Array<string>> = []; // Cria um array para armazenar a letra da música
+                
+                //Busca a letra da música e separa em um array
+                letraContainer.each((_, elem) => {
+                    let estrofe = pag(elem); //Busca a estrofe
+                    let versos = (estrofe.html() as string).split("<br>"); //Separa a estrofe em versos
+                    letra.push(versos); //Adiciona os versos no array letra
+                });
 
-        let letra: Array<Array<string>> = []; // Cria um array para armazenar a letra da música
-        const titulo = pag("article div.cnt-head_title h1").text(); //Busca o título da música
-        
-        //Busca a letra da música e separa em um array
-        pag("div#js-lyric-cnt div.cnt-letra p").each((_, elem) => {
-            let estrofe = pag(elem); //Busca a estrofe
-            let versos = estrofe.html()?.split("<br>") as string[]; //Separa a estrofe em versos
-            letra.push(versos); //Adiciona os versos no array letra
-        });
-
-        // Retorna o título e a letra da música
-        return res.json({
-            titulo: titulo,
-            letra: letra,
-        });
+                // Retorna o título e a letra da música
+                return res.json({
+                    titulo: titulo,
+                    letra: letra,
+                });
+            })
+            .catch((err: Error) => {
+                return "Erro ao definir a URL: " + err.message; // Retorna o erro
+            });
     }
 }
