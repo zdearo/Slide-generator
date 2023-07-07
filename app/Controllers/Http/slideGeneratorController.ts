3d9a1fd215743d0dbac092c11b19ee630693aa3f
Adryel Dearo <adryelzd77@outlook.com>
2023-07-06 23:50:55 -0300

    Adicionando a função de criar slide (Sem formatação)

--- app/Controllers/Http/slideGeneratorController.ts
+++ app/Controllers/Http/slideGeneratorController.ts
@@ -1,17 +1,31 @@
-import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import type { HttpContextContract } from "@ioc:Adonis/Core/HttpContext";
+import PPTX from "nodejs-pptx";
+let pptx = new PPTX.Composer();
 
 export default class SlideGeneratorsController {
-  public async index({}: HttpContextContract) {}
 
-  public async create({}: HttpContextContract) {}
+    public async criarApresentacao({ request: req, response: res }: HttpContextContract) {
+        let musica = await req.all();
+        
+        await pptx.compose(pres => {
+            pres.layout('LAYOUT_16x9');
+            musica.letra.forEach((estrofe: Array<string>) => {
+                pres.addSlide(slide => {
+                    slide.addText(text => {
+                        text.value(estrofe.join('\n'))
+                            .x(100)
+                            .y(100)
+                            .fontSize(25)
+                            .autoFit(true)
+                            .textAlign('center')
+                            .textVerticalAlign('center');
 
-  public async store({}: HttpContextContract) {}
+                    });
+                });
+            });
+            pptx.save('./public/pptx/teste.pptx');
+        });
 
-  public async show({}: HttpContextContract) {}
-
-  public async edit({}: HttpContextContract) {}
-
-  public async update({}: HttpContextContract) {}
-
-  public async destroy({}: HttpContextContract) {}
+        return res.download('./public/pptx/teste.pptx');
+    }
 }
--- app/Routes/webScrapingRoutes.ts
+++ app/Routes/webScrapingRoutes.ts
@@ -1,3 +1,5 @@
 import Route from '@ioc:Adonis/Core/Route';
+import webScrapingController from 'App/Controllers/Http/webScrapingController';
 
 Route.post('/getLetra', 'webScrapingController.getLetra');
+Route.post('/criarApresentacao', 'slideGeneratorController.criarApresentacao');
