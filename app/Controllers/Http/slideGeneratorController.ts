4179663bdcc25f10a8bd181f03943be5cef38ad8
Adryel Dearo <adryelzd77@outlook.com>
2023-07-11 16:13:01 -0300

    Substituindo o módulo para geração dos slides

--- app/Controllers/Http/slideGeneratorController.ts
+++ app/Controllers/Http/slideGeneratorController.ts
@@ -1,31 +1,10 @@
 import type { HttpContextContract } from "@ioc:Adonis/Core/HttpContext";
-import PPTX from "nodejs-pptx";
-let pptx = new PPTX.Composer();
 
 export default class SlideGeneratorsController {
 
     public async criarApresentacao({ request: req, response: res }: HttpContextContract) {
         let musica = await req.all();
         
-        await pptx.compose(pres => {
-            pres.layout('LAYOUT_16x9');
-            musica.letra.forEach((estrofe: Array<string>) => {
-                pres.addSlide(slide => {
-                    slide.addText(text => {
-                        text.value(estrofe.join('\n'))
-                            .x(100)
-                            .y(100)
-                            .fontSize(25)
-                            .autoFit(true)
-                            .textAlign('center')
-                            .textVerticalAlign('center');
-
-                    });
-                });
-            });
-            pptx.save('./public/pptx/teste.pptx');
-        });
-
-        return res.download('./public/pptx/teste.pptx');
+        
     }
 }
