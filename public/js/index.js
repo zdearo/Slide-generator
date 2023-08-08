82c88f9155dbb98112a74f255b093c1036ed8940
Adryel Dearo <adryelzd77@outlook.com>
2023-08-07 21:34:18 -0300

    Refatorando código

--- public/js/index.js
+++ public/js/index.js
@@ -1,35 +1,59 @@
+async function fetchLetra(url) {
+    try {
+        const response = await fetch("/getLetra", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({ url }),
+        });
+
+        const data = await response.json();
+
+        if (data.error) {
+            throw new Error(data.error);
+        }
+
+        return data;
+    } catch (error) {
+        throw new Error("Erro ao obter a letra da música: " + error.message);
+    }
+}
+
+async function fetchApresentacao(data) {
+    try {
+        const response = await fetch("/criarApresentacao", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(data),
+        });
+
+        const blob = await response.blob();
+        return blob;
+    } catch (error) {
+        throw new Error("Erro ao criar a apresentação: " + error.message);
+    }
+}
+
+async function createApresentacao() {
+    try {
+        const url = document.getElementById("url").value;
+        const data = await fetchLetra(url);
+
+        const blob = await fetchApresentacao(data);
+
+        let link = document.createElement("a");
+        link.href = window.URL.createObjectURL(blob);
+        link.download = data.titulo + ".pptx";
+        link.click();
+    } catch (error) {
+        console.log(error.message);
+    }
+}
+
 document.getElementById("urlForm").addEventListener("submit", (event) => {
     event.preventDefault();
-
-    const url = document.getElementById("url").value;
-    let musica;
-
-    fetch("/getLetra", {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json",
-        },
-        body: JSON.stringify({ url }),
-    }).then((response) => {
-        response.json().then((data) => {
-            if (data.error) {
-                console.log(data.error);
-            } else {
-                fetch("/criarApresentacao", {
-                    method: "POST",
-                    headers: {
-                        "Content-Type": "application/json",
-                    },
-                    body: JSON.stringify(data),
-                }).then((response) => {
-                    response.blob().then((blob) => {
-                        let link = document.createElement("a");
-                        link.href = window.URL.createObjectURL(blob);
-                        link.download = data.titulo + ".pptx";
-                        link.click();
-                    });
-                });
-            }
-        });
-    });
+    createApresentacao();
 });
